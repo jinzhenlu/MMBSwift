4c125d61f43a6c565e7ab99d0f516af3d870194b
jinzhenlu <805192369@qq.com>
2019-07-29 15:05:16 +0800

    Initial Commit

---
+++ MMBSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MMBSwift
+//
+//  Created by jinzhenlu on 2019/7/29.
+//  Copyright © 2019 jinzhenlu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
